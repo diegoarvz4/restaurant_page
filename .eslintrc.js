0ae8ba770c4d222136f08d01c2c5a5c3d892dc85
DiegoArvz <darvz.gtz@gmail.com>
2019-10-09 15:23:02 -0500

    more linting fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,11 @@ module.exports = {
   extends: 'eslint:recommended',
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  rules: {}
+  rules: {},
 };
